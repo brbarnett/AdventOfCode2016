400d9dc2760d92022aba32d8dadda4404d925b50
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-12-04 16:32:04 -0600

    Better JS

--- 4b.js
+++ 4b.js
@@ -7,17 +7,15 @@ const realRoomPattern = new RegExp(/([-a-z]+?)(?:-)([0-9]+)(?:\[)(\w+)(?:\])/i);
 
 const room = _(input.split('\n'))
     .chain()
-    .map(roomName => parseRoom(roomName)) // convert to rooms
-    .filter(room => roomIsReal(room)) // only take real rooms
-    .map(room => {
-        room.decrypted = _(room.name.split(''))
+    .map(parseRoom) // convert to rooms
+    .filter(roomIsReal) // only take real rooms
+    .map(room => Object.assign(room, {
+        decrypted: _(room.name.split(''))
             .chain()
             .map(letter => shiftCharacter(letter, room.sectorId))   // shift characters
             .join('')   // join char array to string
-            .value();
-
-        return room;
-    })
+            .value()
+    }))
     .filter(x => _.includes(x.decrypted, 'north'))  // check for a room that includes 'north' in the name
     .head() // get first 
     .value();
@@ -55,12 +53,12 @@ function roomIsReal(room) {
 }
 
 // lowercase a-z ranges from 97-122
-function shiftCharacter(char, shift){
-    if(char === '-') return ' ';    // '-' goes to space
+function shiftCharacter(char, shift) {
+    if (char === '-') return ' ';    // '-' goes to space
 
     var ascii = char.charCodeAt(0); // get ascii code
     ascii += shift % 26;    // shift chars, use mod 26 to wrap
-    if(ascii > 122)
+    if (ascii > 122)
         ascii -= 26;    // ensure within range
 
     return String.fromCharCode(ascii);  // translate char code to ascii
