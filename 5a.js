22d8554ece64f4cc6dcd278809a427b48e1954d9
Brandon R Barnett <BBarnett@rightpoint.com>
2016-12-05 11:07:54 -0600

    Finished 5a

--- 5a.js
+++ 5a.js
@@ -1,4 +1,16 @@
 const _ = require('lodash');
+const cryptoJs = require('crypto-js')
 
 const input = 'ffykfhsq';
 
+var password = '';
+var i = 0;
+while (password.length < 8) {
+    const hash = cryptoJs.MD5(input + i++).toString(cryptoJs.enc.Base16); // get hash. I've hidden an increment in here, too
+    if (_.startsWith(hash, '00000')) {   // check that hash begins with '00000'
+        console.log('Found character', hash[5]);
+        password += hash[5]; // get 6th character (base 0, so index is 5)
+    }
+}
+
+console.log('Result:', password);
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // this is for debugging in vs code, which requires an entry point
-require('./4b.js');
+require('./5a.js');
