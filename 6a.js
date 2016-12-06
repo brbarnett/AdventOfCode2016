f7c553f62b4366ccd7a9c5120fac7f24bd9d544f
Brandon R Barnett <BBarnett@rightpoint.com>
2016-12-06 08:42:33 -0600

    Finished 6a

---
+++ 6a.js
@@ -0,0 +1,27 @@
+const _ = require('lodash');
+
+const fileService = require('./core/fileService');
+
+const input = fileService.getFileContents('./6.dat');
+
+const message = _(input)
+    .chain()
+    .split('\r\n')  // split into lines
+    .map(x => x.split(''))  // split into char arrays
+    .unzip()    // transpose
+    .map(x => _.groupBy(x)) // group so we can count by character
+    .map(x => _(x)
+        .chain()
+        .map((arr, prop) => ({
+            letter: prop,
+            count: arr.length
+        }))
+        .orderBy('count', 'desc')
+        .head()
+        .value()
+    )
+    .map(x => x.letter) // get 'letter'
+    .join('')   // join for answer
+    .value();
+
+console.log('Result:', message);
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // this is for debugging in vs code, which requires an entry point
-require('./5b.js');
+require('./6a.js');
